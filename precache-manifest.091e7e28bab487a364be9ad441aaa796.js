64b84d128139c7b79d573c407a641536b9ab05c4
Nathan Griffiths <ngriffiths@postmedia.com>
2019-08-20 12:24:24 -0700

    Updates

---
+++ precache-manifest.091e7e28bab487a364be9ad441aaa796.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118b400494d05e3d643151ccf9d5533e",
+    "url": "/beer-bike-tour/index.html"
+  },
+  {
+    "revision": "6ac5290fe3ce8bd3d32c",
+    "url": "/beer-bike-tour/static/css/2.80c8bb7a.chunk.css"
+  },
+  {
+    "revision": "8d173029aa036b6f85df",
+    "url": "/beer-bike-tour/static/css/main.6e311a62.chunk.css"
+  },
+  {
+    "revision": "6ac5290fe3ce8bd3d32c",
+    "url": "/beer-bike-tour/static/js/2.74f7a588.chunk.js"
+  },
+  {
+    "revision": "8d173029aa036b6f85df",
+    "url": "/beer-bike-tour/static/js/main.42efa740.chunk.js"
+  },
+  {
+    "revision": "9c11e598f57cce3a954f",
+    "url": "/beer-bike-tour/static/js/runtime~main.616dbffb.js"
+  },
+  {
+    "revision": "2e33a4689c808758157c4d78a8992712",
+    "url": "/beer-bike-tour/static/media/33-acres.2e33a468.jpg"
+  },
+  {
+    "revision": "2003571a9cfcacdb9571aed7a7e897c7",
+    "url": "/beer-bike-tour/static/media/33-brewing.2003571a.jpg"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/beer-bike-tour/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/beer-bike-tour/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/beer-bike-tour/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/beer-bike-tour/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/beer-bike-tour/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/beer-bike-tour/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  },
+  {
+    "revision": "4d6e03334e4e15239f913c13069d1232",
+    "url": "/beer-bike-tour/static/media/andina.4d6e0333.jpg"
+  },
+  {
+    "revision": "e9fcc188dc8dae49939544897c0f79a4",
+    "url": "/beer-bike-tour/static/media/big-rock.e9fcc188.jpg"
+  },
+  {
+    "revision": "66d457abd3f58e18d32360d81a05128a",
+    "url": "/beer-bike-tour/static/media/bomber.66d457ab.jpg"
+  },
+  {
+    "revision": "2e6551af43d054c65125ef6602d94925",
+    "url": "/beer-bike-tour/static/media/brassneck.2e6551af.jpg"
+  },
+  {
+    "revision": "fe3ec479bbb98a87199d33564503cbc1",
+    "url": "/beer-bike-tour/static/media/brewhall.fe3ec479.jpg"
+  },
+  {
+    "revision": "19216b5cd1ba70d23acabf153068876f",
+    "url": "/beer-bike-tour/static/media/callister-01.19216b5c.jpg"
+  },
+  {
+    "revision": "0042030ce6fa47de472923e8dcd73a02",
+    "url": "/beer-bike-tour/static/media/callister.0042030c.jpg"
+  },
+  {
+    "revision": "c80137c78c1cc1100cd0a46aa64a32fa",
+    "url": "/beer-bike-tour/static/media/coal-harbour-01.c80137c7.jpg"
+  },
+  {
+    "revision": "69f9da3a2c719cd1a3bfea5a008a9fdb",
+    "url": "/beer-bike-tour/static/media/coal-harbour.69f9da3a.jpg"
+  },
+  {
+    "revision": "03e9233eceae670f1a05eb5119cf66e0",
+    "url": "/beer-bike-tour/static/media/east-van.03e9233e.jpg"
+  },
+  {
+    "revision": "8de7e4df4fe151dd973009e3829c79a1",
+    "url": "/beer-bike-tour/static/media/electric-bicycle.8de7e4df.jpg"
+  },
+  {
+    "revision": "e64e139fd004f5f30ba929631ed846b4",
+    "url": "/beer-bike-tour/static/media/faculty.e64e139f.jpg"
+  },
+  {
+    "revision": "f7b4035cfb8744bcbf3cac13fc16cc74",
+    "url": "/beer-bike-tour/static/media/hop-circuit.f7b4035c.jpg"
+  },
+  {
+    "revision": "697ebdce49034867d895a9637a124de3",
+    "url": "/beer-bike-tour/static/media/luppolo.697ebdce.jpg"
+  },
+  {
+    "revision": "9c6dae48badba0d9ead0202934031f25",
+    "url": "/beer-bike-tour/static/media/main-street.9c6dae48.jpg"
+  },
+  {
+    "revision": "463a701ebea4ff21cdee90f17be881a3",
+    "url": "/beer-bike-tour/static/media/off-the-rail.463a701e.jpg"
+  },
+  {
+    "revision": "d2d5fcff0abcb081a58eab7e37d9c087",
+    "url": "/beer-bike-tour/static/media/parallel49.d2d5fcff.jpg"
+  },
+  {
+    "revision": "bd9c11ddadb95d93a24983bff440755c",
+    "url": "/beer-bike-tour/static/media/postmark.bd9c11dd.jpg"
+  },
+  {
+    "revision": "f8832191a3fe5bc35108018c1fb09631",
+    "url": "/beer-bike-tour/static/media/powell.f8832191.jpg"
+  },
+  {
+    "revision": "d3f43e9949f0fc3a17604130e51c7e9f",
+    "url": "/beer-bike-tour/static/media/rb.d3f43e99.jpg"
+  },
+  {
+    "revision": "b4b8b147018817573840acc75e6aee50",
+    "url": "/beer-bike-tour/static/media/red-truck.b4b8b147.jpg"
+  },
+  {
+    "revision": "9ddd5161acd698281f071ca47c23de29",
+    "url": "/beer-bike-tour/static/media/steamworks.9ddd5161.jpg"
+  },
+  {
+    "revision": "4b9c9c29b2423d597d7707ad42dd4d2c",
+    "url": "/beer-bike-tour/static/media/storm.4b9c9c29.jpg"
+  },
+  {
+    "revision": "4022753be7692610c78e9cbb816122be",
+    "url": "/beer-bike-tour/static/media/strange-fellows.4022753b.jpg"
+  },
+  {
+    "revision": "3b628a56efa16c325796e998438e8299",
+    "url": "/beer-bike-tour/static/media/strathcona.3b628a56.jpg"
+  },
+  {
+    "revision": "781fce9da2548ce5945eb389e496980d",
+    "url": "/beer-bike-tour/static/media/yaletown.781fce9d.jpg"
+  }
+]);
